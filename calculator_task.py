110dde6f99b3b83209a96f26ee86310bcdc2e010
iwparry <iwparry@hotmail.co.uk>
2023-02-01 15:38:17 +0000

    calculator function added

--- calculator_task.py
+++ calculator_task.py
@@ -2,28 +2,45 @@
 
 # Add
 
-def add(num1: int, num2: int) -> int:
-    return num1 + num2
+def add(num1, num2) -> int:   # currently works only for integers
+    return int(num1) + int(num2)
 print(add(2, 3)) # prints 5
 
 # Subtract
 
-def subtract(num1: int, num2: int) -> int:
-    return num1 - num2
+def subtract(num1, num2) -> int:
+    return int(num1) - int(num2)
 print(subtract(4, 2)) # prints 2
 
 # Multiply
 
-def multiply(num1: int, num2: int) -> int:
-    return num1 * num2
+def multiply(num1, num2) -> int:
+    return int(num1) * int(num2)
 print(multiply(3, 4)) # prints 12
 
 # Divide
 
-def divide(num1: int, num2: int) -> float:
-    if num2 == 0:
-        print("Cannot divide by 0")
+def divide(num1, num2) -> float:
+    if int(num2) == 0:
+        return "Cannot divide by 0"
     else:
-        return num1 / num2
+        return int(num1) / int(num2)
 print(divide(6, 2)) # prints 3.0
 
+# Main calculator body
+def calculator():
+    num1 = input("Pick a number: ")
+    num2 = input("Pick a second number: ")
+    operation = input("Hello! What would you like to do? (+, -, *, /): ")
+    if operation == '+':
+        return add(num1, num2)
+    elif operation == '-':
+        return subtract(num1, num2)
+    elif operation == '*':
+        return multiply(num1, num2)
+    elif operation == '/':
+        return divide(num1, num2)
+    else:
+        return "You have not selected a valid arithmetic operator. Please try again."
+
+print(calculator())
